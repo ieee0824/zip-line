10058489864d6d9a3c93a9f13dcf6c253f90203c
ieee0824 <ieee0824x@gmail.com>
2019-12-28 20:28:28 +0900

    add version

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,14 @@
-docker build -t zipl .
+docker build --no-cache=true -t zipl .
+
+ZIPL_VERSION=$(docker run zipl cat VERSION)
+echo $ZIPL_VERSION
 
 echo "drawin build"
 mkdir -p macOS
-docker run -e GOOS=darwin -v $PWD/macOS:/tmp/build zipl go build -o /tmp/build/zipl cmd/zipl/main.go
+docker run -e GOOS=darwin -v $PWD/macOS:/tmp/build zipl go build -ldflags "-X main.version=${ZIPL_VERSION}" -o /tmp/build/zipl cmd/zipl/main.go
 chown -R $SUDO_USER:$SUDO_USER macOS
 
 echo "linux build"
 mkdir -p linux
-docker run -e GOOS=linux -v $PWD/linux:/tmp/build zipl go build -o /tmp/build/zipl cmd/zipl/main.go
+docker run -e GOOS=linux -v $PWD/linux:/tmp/build zipl go build -ldflags "-X main.version=${ZIPL_VERSION}" -o /tmp/build/zipl cmd/zipl/main.go
 chown -R $SUDO_USER:$SUDO_USER linux
